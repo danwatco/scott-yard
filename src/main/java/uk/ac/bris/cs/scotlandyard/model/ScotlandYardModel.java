bd854e65a4524ab780a1c841977db673f5357c76
Dan Watkinson <dan.watco@gmail.com>
2019-02-28 14:46:01 +0000

    Checking for mising tickets

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,9 +1,7 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singletonList;
@@ -11,10 +9,8 @@ import static java.util.Collections.unmodifiableCollection;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Collections.unmodifiableSet;
 import static java.util.Objects.requireNonNull;
-import java.util.ArrayList;
+
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -73,7 +69,21 @@ public class ScotlandYardModel implements ScotlandYardGame {
 			}
 			set.add(c.location);
 		}
-		
+
+		for (PlayerConfiguration c : configurations){
+			if(c.colour != BLACK){
+				if(c.tickets.get(DOUBLE) != 0 || c.tickets.get(SECRET) != 0){
+					throw new IllegalArgumentException("Detective has wrong tickets");
+				}
+			}
+			for(Ticket t : Ticket.values()){
+				if(c.tickets.containsKey(t)){
+				} else {
+					throw new IllegalArgumentException("Missing tickets");
+				}
+			}
+		}
+
 
 	}
 
